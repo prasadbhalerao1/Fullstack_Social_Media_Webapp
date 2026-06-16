3a12a098dc277e25ff025631a7acf9cc3375f05f
Prasad Bhalerao <prasad9a38@gmail.com>
2026-06-16 22:32:54 +0530

    hook up socket.io to express server using http.createServer

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
 import "dotenv/config";
+import { createServer } from "http";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
 import reelRouter from "./routes/reel.routes.js";
 import storyRouter from "./routes/story.route.js";
+import messageRouter from "./routes/message.routes.js";
 
 import connectDB from "./config/db.js";
+import { initSocket } from "./socket/socket.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsInstance = cors({
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 });
 
 app.use(corsInstance);
@@ -27,13 +35,16 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/reel", reelRouter);
 app.use("/api/v1/story", storyRouter);
+app.use("/api/v1/message", messageRouter);
+
+app.get("/", (_req, res) => res.send("API is running..."));
+
+// Use http.createServer so Socket.IO can share the same port as Express
+const httpServer = createServer(app);
+initSocket(httpServer);
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
