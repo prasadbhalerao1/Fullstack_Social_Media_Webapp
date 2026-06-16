9f0a34250d2e36a7b5b6ca8c85cbfdd95154d9b1
Prasad Bhalerao <prasad9a38@gmail.com>
2026-06-17 01:00:30 +0530

    feat: implement ProtectedRoute wrapper and protect authenticated pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Home from "@/pages/Home.jsx";
 import Profile from "@/pages/Profile.jsx";
 import Login from "@/pages/Login.jsx";
@@ -7,9 +7,28 @@ import Explore from "@/pages/Explore.jsx";
 import Reels from "@/pages/Reels.jsx";
 import Message from "@/pages/Message.jsx";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Loader2 } from "lucide-react";
 import { getCurrentUser } from "./redux/slices/userSlice.js";
 import { SocketProvider } from "@/context/SocketContext.jsx";
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useSelector((state) => state.user);
+
+  if (loading) {
+    return (
+      <div className="bg-black flex text-white min-h-screen justify-center items-center">
+        <Loader2 className="animate-spin text-white w-10 h-10" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 function App() {
   const dispatch = useDispatch();
@@ -18,11 +37,11 @@ function App() {
   }, [dispatch]);
 
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/explore", element: <Explore /> },
-    { path: "/reels", element: <Reels /> },
-    { path: "/chats", element: <Message /> },
-    { path: "/profile/:id", element: <Profile /> },
+    { path: "/", element: <ProtectedRoute><Home /></ProtectedRoute> },
+    { path: "/explore", element: <ProtectedRoute><Explore /></ProtectedRoute> },
+    { path: "/reels", element: <ProtectedRoute><Reels /></ProtectedRoute> },
+    { path: "/chats", element: <ProtectedRoute><Message /></ProtectedRoute> },
+    { path: "/profile/:id", element: <ProtectedRoute><Profile /></ProtectedRoute> },
     { path: "/login", element: <Login /> },
     { path: "/signup", element: <Login /> },
     { path: "/register", element: <Login /> },
